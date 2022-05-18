b7ff759a215ff9737aba077157d5717d20af57c0
Regno-WhatsAppUser <88800877+Regno-WhatsAppUser@users.noreply.github.com>
2022-05-18 20:06:35 +0200

    ArrayList query whether is empty

--- src/main/java/de/whatsappuser/instanceapi/shop/inventory/ShopInventory.java
+++ src/main/java/de/whatsappuser/instanceapi/shop/inventory/ShopInventory.java
@@ -1,6 +1,5 @@
 package de.whatsappuser.instanceapi.shop.inventory;
 
-import de.whatsappuser.instanceapi.InstanceCore;
 import de.whatsappuser.instanceapi.shop.item.ShopItem;
 import lombok.Getter;
 import org.bukkit.Bukkit;
@@ -27,15 +26,12 @@ public class ShopInventory {
     public Inventory getInventory() {
         Inventory inventory = Bukkit.createInventory(null, this.inventorySize, this.inventoryName);
         for (int i = 0; i < inventory.getSize(); i++) {
-            ItemStack item = this.shopItems.get(i).getItem();
-            inventory.setItem(this.shopItems.get(i).getInventorySlot(), item);
+            if(!this.shopItems.isEmpty()) {
+                ItemStack item = this.shopItems.get(i).getItem();
+                inventory.setItem(this.shopItems.get(i).getInventorySlot(), item);
+            }
+            return inventory;
         }
         return inventory;
-    }
-
-    public Inventory getInventory(int inventoryId) {
-        if(!InstanceCore.getInstance().getShopManager().getShopInventories().containsKey(inventoryId)) return null;
-
-        return InstanceCore.getInstance().getShopManager().getShopInventories().get(inventoryId).getInventory();
     }
 }
