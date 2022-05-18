6bf3e4c887133c5aded051c5f58eb7254b39cc71
Regno-WhatsAppUser <88800877+Regno-WhatsAppUser@users.noreply.github.com>
2022-05-18 20:08:12 +0200

    changes for performance

--- src/main/java/de/whatsappuser/instanceapi/InstanceCore.java
+++ src/main/java/de/whatsappuser/instanceapi/InstanceCore.java
@@ -3,7 +3,10 @@ package de.whatsappuser.instanceapi;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.LongSerializationPolicy;
-import de.whatsappuser.instanceapi.configuration.InstanceConfig;
+import de.whatsappuser.instanceapi.command.CreateShopCommand;
+import de.whatsappuser.instanceapi.command.ShowInventoryCommand;
+import de.whatsappuser.instanceapi.configuration.Config;
+import de.whatsappuser.instanceapi.configuration.ShopInventory;
 import de.whatsappuser.instanceapi.serializer.Persist;
 import de.whatsappuser.instanceapi.shop.ShopManager;
 import lombok.Getter;
@@ -19,18 +22,24 @@ public class InstanceCore extends JavaPlugin {
 
     private Persist persist;
 
-    private InstanceConfig instanceConfig;
+    private Config instanceConfig;
+    private ShopInventory shopInventory;
+
     @Override
     public void onLoad() {
         instance = this;
         this.gson = new GsonBuilder().setPrettyPrinting().serializeNulls().setLongSerializationPolicy(LongSerializationPolicy.STRING).create();
         this.shopManager = new ShopManager();
-        this.persist = new Persist(this);
+        if(!getDataFolder().exists()) getDataFolder().mkdir();
     }
 
     @Override
     public void onEnable() {
-
+        this.persist = new Persist(this);
+        loadConfig();
+        saveConfig();
+        getCommand("createShop").setExecutor(new CreateShopCommand(this));
+        getCommand("showinv").setExecutor(new ShowInventoryCommand(this));
     }
 
     @Override
@@ -43,10 +52,12 @@ public class InstanceCore extends JavaPlugin {
     }
 
     public void loadConfig() {
-        this.instanceConfig = this.persist.getFile(InstanceConfig.class).exists() ? this.persist.load(InstanceConfig.class) : new InstanceConfig();
+        this.instanceConfig = this.persist.getFile(Config.class).exists() ? this.persist.load(Config.class) : new Config();
+        this.shopInventory = this.persist.getFile(ShopInventory.class).exists() ? this.persist.load(ShopInventory.class) : new ShopInventory(new de.whatsappuser.instanceapi.shop.inventory.ShopInventory(0, 27, "ExampleInventory"));
     }
 
     public void saveConfig() {
         if(this.instanceConfig != null) this.persist.save(this.instanceConfig);
+        if(this.shopInventory != null) this.persist.save(this.shopInventory);
     }
 }
