b5120641f40ce78702147850f847e15261f79961
Regno-WhatsAppUser <88800877+Regno-WhatsAppUser@users.noreply.github.com>
2022-05-20 13:23:34 +0200

    initial commit

---
+++ src/main/java/de/whatsappuser/instanceapi/config/Config.java
@@ -0,0 +1,7 @@
+package de.whatsappuser.instanceapi.config;
+
+public class Config {
+
+    public String prefix = "§eFramework §8| §7";
+    public boolean sendErrorReports = true;
+    public boolean useOnlineDatabase = false;}
---
+++ src/main/java/de/whatsappuser/instanceapi/config/Messages.java
@@ -0,0 +1,8 @@
+package de.whatsappuser.instanceapi.config;
+
+public class Messages {
+
+    public String noPermission = "*prefifx* &cYou are not authorized to do this";
+    public String mustBeAPlayer = "*prefifx* &cYou must be a player";
+    public String commandHelp = "*prefix* &ecommands help";
+}
