14d494e5397df8a2505992d8e3562993f70e9c31
Regno-WhatsAppUser <88800877+Regno-WhatsAppUser@users.noreply.github.com>
2022-05-20 17:00:32 +0200

    new messages for tests

--- src/main/java/de/whatsappuser/instanceapi/config/Messages.java
+++ src/main/java/de/whatsappuser/instanceapi/config/Messages.java
@@ -5,4 +5,11 @@ public class Messages {
     public String noPermission = "*prefifx* &cYou are not authorized to do this";
     public String mustBeAPlayer = "*prefifx* &cYou must be a player";
     public String commandHelp = "*prefix* &ecommands help";
+    public String playerNotOnline = "*prefix*§cPlayer not Found.";
+    public String reloaded = "*prefix* &aconfiguration reloaded.";
+    public String flyDisabled = "*prefix*&cYour FlyMode is now disabled.";
+    public String flyEnabled = "*prefix*&aYour FlyMode is now enabled.";
+    public String targetFlyDisabled = "*prefix*&c*target* FlyMode is now disabled.";
+    public String targetFlyEnabled = "*prefix*&a*target* FlyMode is now enabled.";
+    public String toManyArguments = "*prefix*§cYou use to many arguments";
 }
